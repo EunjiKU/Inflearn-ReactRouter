032310be4b914e82928bbe6cb6c96f8e19321850
KUEUNJI <yoo128@easymedia.net>
2024-02-06 23:04:31 +0900

    깃?

--- src/routes/Team.jsx
+++ src/routes/Team.jsx
@@ -1,10 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export default function Team() {
   const { teamId } = useParams();
+  const [searchParams] = useSearchParams();
+  const message = searchParams.get("message");
   return (
     <div>
       <h3>Team: {teamId}</h3>
+      {message && <h4>{message}</h4>}
     </div>
   )
 }
